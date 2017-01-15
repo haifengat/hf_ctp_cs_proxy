28edd7baae26913fa96047dd2296f5a263b13aef
hubert.wu <hubert28@qq.com>
2017-01-15 20:01:06 +0800

    采用20160628版本封装，支持期权。支持winform属性绑定自动更新。

---
+++ CTPTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaiFeng
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestTrade tt = new TestTrade();
+			tt.Run();
+			Console.WriteLine("Press any key to continue . . . ");
+			Console.ReadKey(true);
+
+			TestQuote tq = new TestQuote();
+			tq.Run();
+			Console.WriteLine("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
